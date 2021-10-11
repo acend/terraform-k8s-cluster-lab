d618028428073a1724ca496c06caa604b55fb49d
Sebastain Plattner <sebastian.plattner@gmail.com>
2021-10-11 08:33:57 +0200

    use master url for webshell helm repo

--- modules/training-cluster/modules/webshell/variables.tf
+++ modules/training-cluster/modules/webshell/variables.tf
@@ -4,7 +4,7 @@ variable "rancher_training_project" {
 
 variable "chart-repository" {
     type = string
-    default = "https://github.com/acend/webshell-env/raw/helm-chart/deploy/charts/webshell-0.1.0.tgz"
+    default = "https://github.com/acend/webshell-env/raw/main/deploy/charts/webshell-0.1.0.tgz"
 }
 variable "student-name" {
     type = string

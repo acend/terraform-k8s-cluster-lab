64edbdb62e3b3612488d54520f4c475b7b1858ea
Sebastain Plattner <sebastian.plattner@gmail.com>
2021-11-02 16:06:07 +0100

    some cleanup and comments

--- modules/training-cluster/outputs.tf
+++ modules/training-cluster/outputs.tf
@@ -2,6 +2,11 @@ output "vip_address" {
   value = replace(cloudscale_floating_ip.vip-v4.network, "/32", "")
 }
 
+output "vip_address_v6" {
+  value = replace(cloudscale_floating_ip.vip-v6.network, "/128", "")
+}
+
+
 output "webshell-links" {
   value = module.webshell.*.student-direct-webshelllink
 }

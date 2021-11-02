39f0e93189ca5c147e31cc75a8b17ae394092647
Sebastain Plattner <sebastian.plattner@gmail.com>
2021-11-02 14:25:00 +0100

    some debug

--- modules/training-cluster/modules/argocd/outputs.tf
+++ modules/training-cluster/modules/argocd/outputs.tf
@@ -1,3 +1,7 @@
 output "admin-secret" {
   value = data.kubernetes_secret.admin-secret.data.password
 }
+
+output "debug-values" {
+  value = nonsensitive(templatefile("${path.module}/manifests/values_account_student.yaml", {count-students = var.count-students, passwords = var.student-passwords}))
+}

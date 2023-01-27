18b335be8646313aae58ff1de3ecb544cb509325
Sebastian Plattner <sebastian.plattner@gmail.com>
2023-01-27 13:45:10 +0100

    kubed is now config-syncer

--- modules/training-cluster/modules/cert-manager/main.tf
+++ modules/training-cluster/modules/cert-manager/main.tf
@@ -38,10 +38,10 @@ resource "helm_release" "kubed" {
   depends_on = [rancher2_namespace.cert-manager]
 
 
-  name       = "kubed"
+  name       = "config-syncer"
   repository = "https://charts.appscode.com/stable/"
-  chart      = "kubed"
-  version    = "v0.12.0"
+  chart      = "config-syncer"
+  version    = "v0.14.0-rc.0"
   namespace  = "kube-system"
 
 }

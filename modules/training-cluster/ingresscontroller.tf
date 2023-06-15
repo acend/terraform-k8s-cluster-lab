7a9a97dfd52ef88cbede12c9c05c348f92e197d3
Sebastian Plattner <plattner@puzzle.ch>
2023-06-15 14:58:50 +0200

    remove nginx ingress controller

--- modules/training-cluster/ingresscontroller.tf
+++ modules/training-cluster/ingresscontroller.tf
@@ -1,13 +1,3 @@
-resource "kubernetes_namespace" "ingress-nginx" {
-
-  depends_on = [
-    time_sleep.wait_for_cluster_ready
-  ]
-  metadata {
-    name = "ingress-nginx"
-  }
-}
-
 resource "kubernetes_namespace" "ingress-haproxy" {
 
   depends_on = [
@@ -17,44 +7,6 @@ resource "kubernetes_namespace" "ingress-haproxy" {
     name = "ingress-haproxy"
   }
 }
-
-resource "helm_release" "ingress-nginx" {
-
-  name       = "ingress-nginx"
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
-  version    = "4.6.1"
-  namespace  = kubernetes_namespace.ingress-nginx.metadata[0].name
-
-  set {
-    name  = "controller.replicaCount"
-    value = "2"
-  }
-
-  set {
-    name  = "controller.ingressClassResource.default"
-    value = true
-  }
-
-  set {
-    name  = "controller.extraArgs.default-ssl-certificate"
-    value = "cert-manager/acend-wildcard"
-  }
-
-}
-
-data "kubernetes_service" "ingress-nginx" {
-
-  depends_on = [
-    helm_release.ingress-nginx
-  ]
-  metadata {
-    name      = "ingress-nginx-controller"
-    namespace = kubernetes_namespace.ingress-nginx.metadata[0].name
-  }
-
-}
-
 resource "helm_release" "ingress-haproxy" {
 
   name       = "ingress-haproxy"

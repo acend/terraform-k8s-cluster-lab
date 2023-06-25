e8ef0f39128fd7572d48359a56be09df9ebe16ad
Sebastian Plattner <plattner@puzzle.ch>
2023-06-25 21:28:09 +0200

    test deployment of trainees with ArgoCD AppSet

---
+++ modules/training-cluster/trainee-env.tf
@@ -0,0 +1,47 @@
+resource "helm_release" "appset-trainee-env" {
+
+  depends_on = [ 
+    helm_release.argocd
+   ]
+
+  name       = "trainee-env"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argocd-apps"
+  namespace  = kubernetes_namespace.argocd.metadata.0.name
+  version    = "1.2.0"
+
+
+  values = [
+    templatefile("${path.module}/manifests/argocd/values_appset-trainee-env.yaml", { studentname-prefix = var.studentname-prefix, count-students = var.count-students }),
+  ]
+
+} 
+
+resource "helm_release" "appset-trainee-webshell" {
+
+  depends_on = [ 
+    helm_release.argocd
+   ]
+
+  name       = "trainee-webshell"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argocd-apps"
+  namespace  = kubernetes_namespace.argocd.metadata.0.name
+  version    = "1.2.0"
+
+
+  values = [
+    templatefile("${path.module}/manifests/argocd/values_appset-trainee-webshell.yaml", 
+    { 
+        studentname-prefix = var.studentname-prefix, 
+        count-students = var.count-students, 
+        cluster_name = var.cluster_name,
+        cluster_domain = var.cluster_domain,
+        passwords = random_password.student-passwords,
+        rbac-enabled = var.webshell-rbac-enabled,
+        dind-persistence-enabled = var.dind-persistence-enabled,
+        theia-persistence-enabled = var.theia-persistence-enabled,
+    }),
+  ]
+
+} 

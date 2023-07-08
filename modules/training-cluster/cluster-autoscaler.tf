86a66bf71d709fa6390020cb668bf6dde7f3b697
Sebastian Plattner <plattner@puzzle.ch>
2023-07-08 18:25:31 +0200

    disable cluster autoscaler

--- modules/training-cluster/cluster-autoscaler.tf
+++ modules/training-cluster/cluster-autoscaler.tf
@@ -1,17 +1,17 @@
-resource "helm_release" "cluster-autoscaler" {
+# resource "helm_release" "cluster-autoscaler" {
 
-  depends_on = [
-    time_sleep.wait_for_cluster_ready
-  ]
+#   depends_on = [
+#     time_sleep.wait_for_cluster_ready
+#   ]
 
-  name       = "cluster-autoscaler"
-  repository = "https://kubernetes.github.io/autoscaler"
-  chart      = "cluster-autoscaler"
-  version    = "9.28.0"
-  namespace  = "kube-system"
+#   name       = "cluster-autoscaler"
+#   repository = "https://kubernetes.github.io/autoscaler"
+#   chart      = "cluster-autoscaler"
+#   version    = "9.28.0"
+#   namespace  = "kube-system"
 
-  values = [
-    "${templatefile("${path.module}/manifests/cluster-autoscaler-values.yaml", {
-    cluster_name = var.cluster_name })}"
-  ]
-}
+#   values = [
+#     "${templatefile("${path.module}/manifests/cluster-autoscaler-values.yaml", {
+#     cluster_name = var.cluster_name })}"
+#   ]
+# }

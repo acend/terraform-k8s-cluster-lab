a809a5fe91039555984a6d95dd282d63e5f6d31e
Sebastian Plattner <plattner@puzzle.ch>
2023-07-08 09:48:48 +0200

    fix api call url in gitea resources

--- modules/training-cluster/gitea_user_repo.tf
+++ modules/training-cluster/gitea_user_repo.tf
@@ -40,7 +40,7 @@ resource "restapi_object" "gitea-repo" {
   provider     = restapi.gitea
   path         = "/api/v1/repo/{repo_owner}/{id}"
   create_path  = "/api/v1/repos/migrate"
-  destroy_path = "7api/v1/repo/{repo_owner}/{id}"
+  destroy_path = "/api/v1/repo/{repo_owner}/{id}"
   data         = "${jsonencode({
     clone_addr = "https://github.com/acend/argocd-training-examples.git"
     private = false

808f1484087896d3230398c440aa287aa0befceb
Sebastian Plattner <plattner@puzzle.ch>
2023-07-13 16:25:33 +0200

    (feat) deploy haproxy with argocd instead of terraform

--- modules/training-cluster/dns.tf
+++ modules/training-cluster/dns.tf
@@ -31,35 +31,3 @@ module "api-aaaa-record" {
   comment = "K8S API for Training Cluster ${var.cluster_name}"
   ttl     = 3600
 }
-
-
-module "ingress-a-record" {
-
-  providers = {
-    restapi = restapi.hosttech_dns
-  }
-
-  source = "./modules/hosttech-dns-record"
-
-  hosttech-dns-zone-id = var.hosttech-dns-zone-id
-
-  type    = "A"
-  name    = "*.${var.cluster_name}.${split(".", var.cluster_domain)[0]}"
-  ipv4    = data.kubernetes_service.ingress-haproxy.status.0.load_balancer.0.ingress.0.ip
-  comment = "Ingress Wildcard for Lab Cluster ${var.cluster_name}"
-}
-
-module "ingress-aaaa-record" {
-  providers = {
-    restapi = restapi.hosttech_dns
-  }
-  source = "./modules/hosttech-dns-record"
-
-  hosttech-dns-zone-id = var.hosttech-dns-zone-id
-
-  type    = "AAAA"
-  name    = "*.${var.cluster_name}.${split(".", var.cluster_domain)[0]}"
-  ipv6    = data.kubernetes_service.ingress-haproxy.status.0.load_balancer.0.ingress.1.ip
-  comment = "Ingress Wildcard for Lab Cluster ${var.cluster_name}"
-
-}

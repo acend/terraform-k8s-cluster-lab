48856b7ed068d93e8f3fe5060bb288594944961d
Sebastian Plattner <plattner@puzzle.ch>
2023-07-13 12:12:37 +0200

    (feat) dont deploy the hosttech secret anymore, will be pushed from acend cluster

--- modules/training-cluster/secrets.tf
+++ modules/training-cluster/secrets.tf
@@ -1,22 +1,3 @@
-resource "kubernetes_secret" "hosttech-secret" {
-  provider = kubernetes.local
-
-  depends_on = [
-    null_resource.wait_for_k8s_api
-  ]
-  metadata {
-    name      = "hosttech-secret"
-    namespace = "kube-system"
-    annotations = {
-      "kubed.appscode.com/sync" = "app=cert-manager"
-    }
-  }
-
-  data = {
-    token = var.hosttech_dns_token
-  }
-}
-
 resource "kubernetes_secret" "hcloud" {
   provider = kubernetes.local
 

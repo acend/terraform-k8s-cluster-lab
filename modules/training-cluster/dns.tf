899b48c83ab7ba25fb8c5fcd0c1a422907b5efb6
Sebastian Plattner <plattner@puzzle.ch>
2023-07-13 12:39:00 +0200

    (feat) also use dns module for api records

--- modules/training-cluster/dns.tf
+++ modules/training-cluster/dns.tf
@@ -1,15 +1,35 @@
-resource "restapi_object" "api-a-record" {
-  provider     = restapi.hosttech_dns
-  path         = "/api/user/v1/zones/${var.hosttech-dns-zone-id}/records"
-  data         = "{\"type\": \"A\",\"name\": \"api.${var.cluster_name}.${split(".", var.cluster_domain)[0]}\",\"ipv4\": \"${hcloud_load_balancer.lb.ipv4}\",\"ttl\": 3600,\"comment\": \"K8S API for Lab Cluster ${var.cluster_name}\"}"
-  id_attribute = "data/id"
+module "api-a-record" {
+
+  providers = {
+    restapi = restapi.hosttech_dns
+  }
+
+  source = "./modules/hosttech-dns-record"
+
+  hosttech-dns-zone-id = var.hosttech-dns-zone-id
+
+  type    = "A"
+  name    = "api.${var.cluster_name}.${split(".", var.cluster_domain)[0]}"
+  ipv4    = hcloud_load_balancer.lb.ipv4
+  comment = "K8S API for Training Cluster ${var.cluster_name}"
+  ttl     = 300
 }
 
-resource "restapi_object" "api-aaaa-record" {
-  provider     = restapi.hosttech_dns
-  path         = "/api/user/v1/zones/${var.hosttech-dns-zone-id}/records"
-  data         = "{\"type\": \"AAAA\",\"name\": \"api.${var.cluster_name}.${split(".", var.cluster_domain)[0]}\",\"ipv6\": \"${hcloud_load_balancer.lb.ipv6}\",\"ttl\": 3600,\"comment\": \"K8S API Lab Cluster ${var.cluster_name}\"}"
-  id_attribute = "data/id"
+module "api-aaaa-record" {
+
+  providers = {
+    restapi = restapi.hosttech_dns
+  }
+
+  source = "./modules/hosttech-dns-record"
+
+  hosttech-dns-zone-id = var.hosttech-dns-zone-id
+
+  type    = "AAAA"
+  name    = "api.${var.cluster_name}.${split(".", var.cluster_domain)[0]}"
+  ipv6    = hcloud_load_balancer.lb.ipv6
+  comment = "K8S API for Training Cluster ${var.cluster_name}"
+  ttl     = 300
 }
 
 
@@ -27,10 +47,8 @@ module "ingress-a-record" {
   name    = "*.${var.cluster_name}.${split(".", var.cluster_domain)[0]}"
   ipv4    = data.kubernetes_service.ingress-haproxy.status.0.load_balancer.0.ingress.0.ip
   comment = "Ingress Wildcard for Lab Cluster ${var.cluster_name}"
-
-
-
 }
+
 module "ingress-aaaa-record" {
   providers = {
     restapi = restapi.hosttech_dns
